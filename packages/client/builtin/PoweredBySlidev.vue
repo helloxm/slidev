fde1ce9f690bfddf16b3ce4455dbe6d399f20d13
helloxm <fangjtan@gamil.com>
2024-07-02 11:32:58 +0800

    slide 改为水木信奥

--- packages/client/builtin/PoweredBySlidev.vue
+++ packages/client/builtin/PoweredBySlidev.vue
@@ -1,8 +1,8 @@
 <template>
   <span inline-flex items-center>
     <span text-main>Powered by</span>
-    <a href="https://sli.dev" class="!border-none">
-      <img alt="Slidev logo" src="../assets/logo-title-horizontal.png" h-1.5em>
+    <a href="https://oj.noicoding.cn" class="!border-none">
+      水木-信奥
     </a>
   </span>
 </template>
